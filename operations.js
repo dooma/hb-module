d8a501275019b6b9a45d77f06d09483e65c6fb3d
Călin Sălăgean <salagean.calin@gmail.com>
2013-07-16 17:47:26 +0300

    add default operations

--- operations.js
+++ operations.js
@@ -1,1 +1,35 @@
-// operations go here
+model = require('./model.js');
+
+exports.index = function (link) {
+    if (link.data) {
+        console.log(link.data);
+        var response = model.getUsers(link.data, link.send);
+        return;
+    }
+
+    link.send(200, 'No data sent to me');
+};
+
+exports.show = function (link) {
+    if (link.data) {
+        return;
+    }
+
+    link.send(200, 'No data sent to me');
+};
+
+exports.edit = function (link) {
+    if (link.data) {
+        return;
+    }
+
+    link.send(200, 'No data sent to me');
+};
+
+exports.transfer = function (link) {
+    if (link.data) {
+        return;
+    }
+
+    link.send(200, 'No data sent to me');
+};
