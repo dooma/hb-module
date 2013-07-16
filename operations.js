0680f36b9d0af94a78bcf7444b038743993dd7df
Călin Sălăgean <salagean.calin@gmail.com>
2013-07-16 18:24:51 +0300

    add response for each operation

--- operations.js
+++ operations.js
@@ -1,35 +1,63 @@
 model = require('./model.js');
 
 exports.index = function (link) {
-    if (link.data) {
-        console.log(link.data);
-        var response = model.getUsers(link.data, link.send);
+    if (!link.data) {
+        link.send(200, 'No data sent to me');
         return;
     }
 
-    link.send(200, 'No data sent to me');
+    console.log(link.data);
+    model.getUsers(link.data, function (error, data) {
+        if (error) {
+            link.send(400, error);
+        } else {
+            link.send(200, data);
+        }
+    });
 };
 
 exports.show = function (link) {
-    if (link.data) {
+    if (!link.data) {
+        link.send(200, 'No data sent to me');
         return;
     }
 
-    link.send(200, 'No data sent to me');
+    console.log(link.data)
+    model.getUser(link.data, function (error, data) {
+        if (error) {
+            link.send(400, error);
+        } else {
+            link.send(200, data);
+        }
+    }
 };
 
 exports.edit = function (link) {
-    if (link.data) {
+    if (!link.data) {
+        link.send(200, 'No data sent to me');
         return;
     }
 
-    link.send(200, 'No data sent to me');
+    model.editUser(link.data.id, link.data.points, function (error, data) {
+        if (error) {
+            link.send(400, error);
+        } else {
+            link.send(200, data);
+        }
+    }
 };
 
 exports.transfer = function (link) {
-    if (link.data) {
+    if (!link.data) {
+        link.send(200, 'No data sent to me');
         return;
     }
 
-    link.send(200, 'No data sent to me');
+    model.getUser(link.data.ids, link.data.points, function (error, data) {
+        if (error) {
+            link.send(400, error);
+        } else {
+            link.send(200, data);
+        }
+    }
 };
